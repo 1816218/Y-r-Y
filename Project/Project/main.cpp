3aff3f2265a621c0eaa501b52b66eb76476dae1a
mnmtR <1816036@s.asojuku.ac.jp>
2020-05-13 14:03:43 +0900

    main.cpp

--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -1,20 +1,11 @@
-#include "main.h"
+#include <DxLib.h>
+#include "Scene/SceneMng.h"
 
 //メイン処理
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	ChangeWindowMode(TRUE);//ウィンドウ
-	SetWindowText(WINDOW_NAME);
-	SetGraphMode(SCREEN_SIZE_X, SCREEN_SIZE_Y, 16);
-	if (DxLib_Init() == -1) return -1;//初期化と裏画面化
-	SetDrawScreen(DX_SCREEN_BACK);
-	//メインループ
-	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) != 1)
-	{
-		ClsDrawScreen();
-		ScreenFlip();
-	}
+	lpSceneMng.Run();
+
 	DxLib_End();//終了の処理
 	return 0;//ソフトの処理
-
 }
