052d1d2098ccbfab325f44844ea8acb9c22a25c3
mnmtR <1816036@s.asojuku.ac.jp>
2020-05-14 13:02:01 +0900

    ImageMngの追加

--- Project/Project/ImageMng.cpp
+++ Project/Project/ImageMng.cpp
@@ -1,1 +1,27 @@
 #include "ImageMng.h"
+#include <DxLib.h>
+
+ImageMng* ImageMng::s_Instance = nullptr;	//²İ½Àİ½‚·‚é
+
+//-----‰æ‘œ‚Ì“o˜^(‰æ‘œˆê–‡)
+const int& ImageMng::SetID(std::string fileName)
+{
+	if (imgMap.find(fileName) == imgMap.end())
+	{
+		imgMap[fileName] = LoadGraph(fileName.c_str());
+	}
+	return imgMap[fileName];
+}
+//-----‰æ‘œ‚Ì“o˜^(ƒAƒjƒ[ƒVƒ‡ƒ“)
+const VEC_INT& ImageMng::SetID(const std::string& fileName, const Vector2& divSize, const Vector2& divNum)
+{
+	if (imgDivMap.find(fileName) == imgDivMap.end())
+	{
+		//“®“I‚È—v‘f”‚ğİ’è(vectorŒ^”z—ñ)
+		imgDivMap[fileName].reserve(divNum.x * divNum.y);
+
+		LoadDivGraph(fileName.c_str(), divNum.x* divNum.y, divNum.x, divNum.y, divSize.x,divSize.y, &imgDivMap[fileName][0], true);
+	}
+
+	return imgDivMap[fileName];
+}
--- Project/Project/ImageMng.h
+++ Project/Project/ImageMng.h
@@ -1,19 +1,38 @@
 #pragma once
-#include <memory>
+#include <map>
+#include <vector>
+#include <string>
+#include "VECTOR2.h"
+
+#define  IMAGE_ID(N) ImageMng::GetInstance(N)
+using VEC_INT = std::vector<int>;
 
 class ImageMng
 {
 public:
 	static ImageMng& GetInstance()
 	{
-		return *s_Instance;
+		// 実態を持っていないならｺﾝｽﾄﾗｸﾀｰを呼ぶ
+		if (s_Instance == nullptr)
+		{
+			s_Instance = new ImageMng();
+		}
+		return (*s_Instance);	// 実態を返す
 	}
-
-
-private:
-	ImageMng() {}
 	~ImageMng() {}
 
-	static ImageMng s_Instance;
+	//登録
+	const int& SetID(std::string fileName);
+
+	//登録(アニメーション)
+	const VEC_INT& SetID(const std::string & fileName, const Vector2 & divSize, const Vector2 & divNum);
+private:
+	ImageMng();
+
+	//-----画像登録用map
+	std::map<std::string, int> imgMap;	//画像一枚用
+	std::map<std::string, VEC_INT>imgDivMap;	//アニメーション用
+
+	static ImageMng* s_Instance;
 };
 
