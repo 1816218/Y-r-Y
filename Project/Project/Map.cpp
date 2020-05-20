da6984727add228610c826f03fbcbb2632465ebb
mnmtR <1816036@s.asojuku.ac.jp>
2020-05-20 17:33:09 +0900

    ファイルの読み込み

--- Project/Project/Map.cpp
+++ Project/Project/Map.cpp
@@ -6,6 +6,7 @@ Map* Map::s_Instance = nullptr;
 
 Map::Map()
 {
+	LoadDivGraph("image/map.png", 24, 6, 4, 32, 32, Image);
 	Init();
 }
 
@@ -15,27 +16,27 @@ Map::~Map()
 
 void Map::Init()
 {
-	FILE *fp;
-	
-	//ファイルを開く
-	fopen_s(&fp, "Data/map.csv", "r");
+	//FILE *fp;
+	//
+	////ファイルを開く
+	//fopen_s(&fp, "Data/map.csv", "r");
 
-	//ファイルの中身が空なら処理を抜ける
-	if (fp == nullptr)
-	{
-		return;
-	}
+	////ファイルの中身が空なら処理を抜ける
+	//if (fp == nullptr)
+	//{
+	//	return;
+	//}
 
-	//ファイルの読み込み
-	for (int y = 0; y < MAP_CHIP_Y; y++)
-	{
-		for (int x = 0; x < MAP_CHIP_X; x++)
-		{
-			fscanf_s(fp, "%d", &mapData[y][x]);
-		}
-	}
+	////ファイルの読み込み
+	//for (int y = 0; y < MAP_CHIP_Y; y++)
+	//{
+	//	for (int x = 0; x < MAP_CHIP_X; x++)
+	//	{
+	//		fscanf_s(fp, "%d", &mapData[y][x]);
+	//	}
+	//}
 
-	fclose(fp);	//ファイルを閉じる
+	//fclose(fp);	//ファイルを閉じる
 }
 
 void Map::Draw(void)
@@ -44,7 +45,7 @@ void Map::Draw(void)
 	{
 		for (int x = 0; x < MAP_CHIP_X; x++)
 		{
-			DrawGraph(x * 32, y * 32, IMAGE_ID.SetID("image/map.png", { 32,32 }, { 6,4 })[mapData[y][x]], true);
+			DrawGraph(x * 32, y * 32, _image[_mapData[y][x]], true);
 		}
 	}
 }
