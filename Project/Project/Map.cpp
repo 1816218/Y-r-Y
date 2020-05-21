74883b34b2501e99ce221b9e6a191a8a876b7881
mnmtR <1816036@s.asojuku.ac.jp>
2020-05-21 12:58:28 +0900

    マップデータの読み込み

--- Project/Project/Map.cpp
+++ Project/Project/Map.cpp
@@ -4,9 +4,8 @@
 
 Map* Map::s_Instance = nullptr;
 
-Map::Map()
+Map::Map() : _mapChipSize(32,32)
 {
-	LoadDivGraph("image/map.png", 24, 6, 4, 32, 32, _image);
 	Init();
 }
 
@@ -14,21 +13,27 @@ Map::~Map()
 {
 }
 
-void Map::Init()
+void Map::Init(void)
 {
-	FILE *fp;
-	
-	//ファイルを開く
-	fopen_s(&fp, "Data/map.csv", "r");
+	LoadDivGraph("image/map.png", 24, 6, 4, 32, 32, _image);
 
+	//-----ファイルの読み込み
+	FILE* fp;
+	//ファイルを開く
+	fopen_s(&fp, "Data/test.csv", "r");	
 	//ファイルの中身が空なら処理を抜ける
 	if (fp == nullptr)
 	{
 		return;
 	}
-
 	//ファイルの読み込み
-
+	for (int y = 0; y < MAP_CHIP_Y; y++)
+	{
+		for (int x = 0; x < MAP_CHIP_X; x++)
+		{
+			fscanf_s(fp, "%d", &_mapData[y][x]);
+		}
+	}
 	fclose(fp);	//ファイルを閉じる
 }
 
@@ -38,7 +43,7 @@ void Map::Draw(void)
 	{
 		for (int x = 0; x < MAP_CHIP_X; x++)
 		{
-			DrawGraph(x * 32, y * 32, _image[_mapData[y][x]], true);
+			DrawGraph(x * _mapChipSize.x, y * _mapChipSize.y, _image[_mapData[y][x]], true);
 		}
 	}
 }
